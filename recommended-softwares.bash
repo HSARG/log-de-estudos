cbbb2974c0556472478c6d461611664f020d98c4
Henrique <henriquesauerdearaujo@gmail.com>
2023-10-25 15:53:31 -0300

    novo arquivo, com softwares recomendados

---
+++ recommended-softwares.bash
@@ -0,0 +1,5 @@
+##This is a list of free softwares i personally recommend with an small description of them. I will try to keep it in a way that can be downloaded by running the current document as a bash script, since most of them are on popular repositories such as pip or apt.
+
+#Deluge - Deluge is a popular torrent client. It have some minor advantages in comparassion to other alternatives, particularly its hability of being controlled by an web interface and picking up automatically torrent files from a folder.
+
+sudo apt install deluge deluged deluge-web deluge-console;
