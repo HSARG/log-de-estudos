4fd34e222080b8a037c37ff7596a1fe9c3c2be16
Henrique <henriquesauerdearaujo@gmail.com>
2023-11-11 08:35:29 -0300

    AD

--- recommended-softwares.bash
+++ recommended-softwares.bash
@@ -6,26 +6,33 @@
 
 #APT - stands for Advanced Package Tool, and its the standard management system used in debian-based distros like ubuntu. You dont "install" apt, because it comes pre-installed on these systems. However, i do mention it because we need the system up to date to install a lot of the softwares on the listen. This is done using the next command.
 
-sudo apt update; sudo apt upgrade -y;
+sudo apt update; 
+sudo apt upgrade -y;
 
 #Pip - Pip Installs Packages is a software utility that downloads and manages packages from PyPI - the Python Package Index. Pip is a command-line program; when installed, it adds the pip command line to the system. Use the tool to install and manage Python software packages. We will need it to download several softwares on this list. [credits to https://phoenixnap.com/kb/how-to-install-pip-on-ubuntu]
 
-sudo apt install python3-pip -y; sudo pip3 install --upgrade pip;
+sudo apt install python3-pip -y; 
+sudo pip3 install --upgrade pip;
+pip install pip-review;
+pip-review --local --auto;
 
 #Flatpak - Formerly known as xdg-app, is a utility for software deployment and package management for Linux. 
 
 sudo apt install flatpak -y; 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
+flatpak update -y;
 
 #Snap - Snap is a package management system and application platform that works across a range of Linux distributions. It allows developers to package their applications and dependencies in a single container, making it easy to distribute and install software.
 
-sudo apt install snapd -y; sudo snap refresh; 
+sudo apt install snapd -y; 
+sudo snap refresh; 
 
 ###List of softwares and their installation procedures.
 
 #7-Zip - This compression/descompression software is an important alternative for winrar, that doesnt not waste your time asking for you to buy a license. 
 
-sudo apt install p7zip-full -y; sudo apt install p7zip-rar -y;
+sudo apt install p7zip-full -y; 
+sudo apt install p7zip-rar -y;
 
 #GNOME Soundconverter - Pretty straightfoward tool to convert all short of audio files to different formats. Particularly usefull for mass convertion.
 
@@ -72,4 +79,8 @@ flatpak install flathub com.mojang.Minecraft -y;
 
 #Kodi - This is certainly the most mature open source media center around. Its focused on TV's, but its also pretty convenient on desktop enviromments, since it can give you tools to watch a sequence of videos by keeping tabs on what you already seen and where did you last stopped.
 
-sudo apt install software-properties-common -y; sudo add-apt-repository -y ppa:team-xbmc/ppa -y; sudo apt install kodi -y;
+sudo apt install software-properties-common -y; 
+sudo add-apt-repository -y ppa:team-xbmc/ppa -y; 
+sudo apt install kodi -y;
+
+
